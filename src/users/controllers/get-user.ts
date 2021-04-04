de64b154c201a94c21baf2d0ce12285cc277d891
Anthony <quereantho@gmail.com>
2021-04-04 11:46:08 +0200

    Fix : add missing await

--- src/users/controllers/get-user.ts
+++ src/users/controllers/get-user.ts
@@ -8,7 +8,7 @@ export default function makeGetUser({findUser}: MakeGetUserOptions) {
     return async function getUser(httpRequest) {
         const {id} = httpRequest.params;
 
-        const user = findUser({id});
+        const user = await findUser({id});
 
         if (user === null) {
             throw new Error("User not found");
