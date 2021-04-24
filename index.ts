7208e04b6d2c53a94d2cbfd7bf0e3b05265a66b9
Anthony <quereantho@gmail.com>
2021-04-24 22:08:22 +0200

    Fix : environment declaration

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { PORT } from "@tas/environment";
 import "module-alias/register";
+import { PORT } from "@tas/environment";
 import app from "./app";
 
 const server = app.listen(PORT, () => {
-    // @ts-ignore
-    const port = server.address().port;
-    console.log("App running on port", port);
-  });
+  // @ts-ignore
+  const port = server.address().port;
+  console.log("App running on port", port);
+});
