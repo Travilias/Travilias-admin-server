9eda857cb72fd75a67d6fecdddecf07ddda14fe9
Anthony <quereantho@gmail.com>
2021-04-24 21:57:55 +0200

    Fix build issues with posts

--- src/posts/controllers/index.ts
+++ src/posts/controllers/index.ts
@@ -1,19 +1,13 @@
-import {findImageById} from "@tas/images/use-cases";
-import makeGetPosts from "@tas/posts/controllers/get-posts";
-import {addPost, findPostById, listPosts, editPost} from "@tas/posts/use-cases";
-import {findUser} from "@tas/users/use-cases";
-import makePostPost from "@tas/posts/controllers/post-post";
 import makeGetPostById from "@tas/posts/controllers/get-post-by-id";
+import makeGetPosts from "@tas/posts/controllers/get-posts";
+import makePostPost from "@tas/posts/controllers/post-post";
 import makePutPost from "@tas/posts/controllers/put-post";
+import { addPost, editPost, findPostById, listPosts } from "@tas/posts/use-cases";
 
 
-function findUserById(id: string) {
-    return findUser({id});
-}
-
-const getPosts = makeGetPosts({listPosts, findImageById, findUserById});
+const getPosts = makeGetPosts({listPosts});
 const postPost = makePostPost({addPost});
-const getPostById = makeGetPostById({findPostById, findImageById, findUserById});
+const getPostById = makeGetPostById({findPostById});
 const putPost = makePutPost({editPost});
 
 export {
@@ -21,4 +15,4 @@ export {
     postPost,
     getPostById,
     putPost
-}
+};
