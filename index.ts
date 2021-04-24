748053819cefb2659ba5458eb2df185109cc4ca6
Anthony <quereantho@gmail.com>
2021-04-24 21:36:11 +0200

    Variablilize port

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
+import { PORT } from "@tas/environment";
 import "module-alias/register";
 import app from "./app";
 
-const server = app.listen(8080, () => {
+const server = app.listen(PORT, () => {
     // @ts-ignore
     const port = server.address().port;
     console.log("App running on port", port);
